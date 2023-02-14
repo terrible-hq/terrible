a59a7145ce470fe29e3e43a2a808c6ee5345308c
Terence Ponce <terenceponce@gmail.com>
2023-02-14 00:58:34 +0000

    Add Credo
    This adds Credo in our dependencies to serve as our linting library.
    
    This also changes the shortcut to mix for formatting and linting the
    codebase.

--- lib/terrible_web/components/layouts.ex
+++ lib/terrible_web/components/layouts.ex
@@ -1,4 +1,6 @@
 defmodule TerribleWeb.Layouts do
+  @moduledoc false
+
   use TerribleWeb, :html
 
   embed_templates "layouts/*"
--- lib/terrible_web/controllers/page_html.ex
+++ lib/terrible_web/controllers/page_html.ex
@@ -1,4 +1,6 @@
 defmodule TerribleWeb.PageHTML do
+  @moduledoc false
+
   use TerribleWeb, :html
 
   embed_templates "page_html/*"
