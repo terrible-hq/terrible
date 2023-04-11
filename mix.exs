65cded3c72b156211a33466002aa23b9fa2a9da2
Terence Ponce <terence@terrible.finance>
2023-04-11 20:20:34 +0100

    Upgrade Elixir to 1.14.4 (#22)
    This also adds a .tool-versions file just to make sure the versions are
    consistent.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Terrible.MixProject do
     [
       app: :terrible,
       version: "0.1.0",
-      elixir: "1.14.3",
+      elixir: "1.14.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
